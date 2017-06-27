e35366126fdad24a6e569c113df6b8387ee8f585
NoeTheNose <noethenose@gmail.com>
2017-06-26 18:20:27 -0700

    Add files via upload

---
+++ IntroToStrings.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class IntroToStrings {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"ayeee");
+	String x="Boo!";
+	String Plumbus=JOptionPane.showInputDialog("Shleem");
+	JOptionPane.showMessageDialog(null,"Whats Up"+Plumbus);
+}
+}
